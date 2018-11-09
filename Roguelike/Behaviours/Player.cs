cda1c3a377718d832631ebc189f6f59aa81df06d
Kian Kane <AmethystArchon@gmail.com>
2018-11-09 18:42:56 +0000

    Add arrow keys controls

--- Roguelike/Behaviours/Player.cs
+++ Roguelike/Behaviours/Player.cs
@@ -1,4 +1,6 @@
 ﻿using Roguelike.EntityBehaviourAction;
+using System;
+using Action = Roguelike.EntityBehaviourAction.Action;
 
 namespace Roguelike.Behaviours
 {
@@ -8,9 +10,38 @@ namespace Roguelike.Behaviours
         {
             if (action.ID == "player_turn")
             {
+                // Clear console input
+                while (Console.KeyAvailable) { Console.ReadKey(true); }
+
+                // Get direction
+                Point direction = Point.zero;
+                do
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.W:
+                        case ConsoleKey.UpArrow:
+                            direction = Point.up;
+                            break;
+                        case ConsoleKey.S:
+                        case ConsoleKey.DownArrow:
+                            direction = Point.down;
+                            break;
+                        case ConsoleKey.D:
+                        case ConsoleKey.RightArrow:
+                            direction = Point.right;
+                            break;
+                        case ConsoleKey.A:
+                        case ConsoleKey.LeftArrow:
+                            direction = Point.left;
+                            break;
+                    }
+
+                } while (direction == Point.zero);
+
                 Action moveAction = new Action("move");
                 moveAction.Parameters.Add("entity", Parent);
-                moveAction.Parameters.Add("direction", Point.right);
+                moveAction.Parameters.Add("direction", direction);
                 EntityManager.Fire(moveAction);
             }
         }
